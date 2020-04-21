de7bf2be3ed209c5da961d5ee63e0f9f12aa9f09
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-04-22 00:24:37 +1200

    Animate to user location on load

--- nav-map/MapScreen.tsx
+++ nav-map/MapScreen.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
-import MapView from "react-native-maps";
+import MapView, { Region } from "react-native-maps";
 import { StyleSheet, View, Dimensions, Platform } from "react-native";
 import * as Location from "expo-location";
 import VehicleMarker from "./VehicleMarker"
 import RouteData from "../types/RouteData"
 import Route from "./Route";
 
-const AUCKLAND_COORDS = {
-    latitude: -37.028454,
-    longitude: 174.921419,
-    latitudeDelta: 0.218087,
-    longitudeDelta: 0.264047,
+const { width, height } = Dimensions.get("window");
+
+const AUCKLAND_REGION: Region = {
+    latitude: -36.8588,
+    longitude: 174.7641,
+    latitudeDelta: 0.1439 * height/width,
+    longitudeDelta: 0.1439,
 };
 
 interface MapScreenProps {
@@ -18,13 +20,15 @@ interface MapScreenProps {
 }
 
 export default function MapScreen({ activeRoutes }: MapScreenProps) {
-    const [region, setRegion] = useState(AUCKLAND_COORDS);
+    const [region, setRegion] = useState(AUCKLAND_REGION);
+    const [queuedRegion, animateToRegion] = useState<Region | null>(null);
     const [map, setMap] = useState<MapView | null>(null);
 
     useEffect(() => {
         (async () => {
+            // TODO: make this a setting
             // check if user has already moved the map
-            if (region !== AUCKLAND_COORDS) return;
+            if (region !== AUCKLAND_REGION) return;
 
             let { status } = await Location.requestPermissionsAsync();
             if (status === "granted") {
@@ -32,7 +36,7 @@ export default function MapScreen({ activeRoutes }: MapScreenProps) {
                     const pos = await Location.getCurrentPositionAsync({});
                     const { latitude, longitude } = pos.coords;
                     const { latitudeDelta, longitudeDelta } = region;
-                    setRegion({
+                    animateToRegion({
                         latitude,
                         longitude,
                         latitudeDelta,
@@ -47,14 +51,18 @@ export default function MapScreen({ activeRoutes }: MapScreenProps) {
         })();
     }, ["runOnce"]);
 
+    if (map !== null && queuedRegion !== null) {
+        map.animateToRegion(queuedRegion);
+        animateToRegion(null);
+    }
+
     return (
         <MapView
             style={styles.mapStyle}
             ref={ref => setMap(ref)}
-            region={region}
+            initialRegion={region}
             onRegionChangeComplete={r => setRegion(r)}
-            provider="google"
-            followsUserLocation={true}>
+            provider="google">
             { activeRoutes.map(r => <Route key={r.shortName} route={r} />) }
         </MapView>
     );
