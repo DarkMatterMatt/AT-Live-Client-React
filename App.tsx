b1e7f8c3f76ba77c0e42f231db18dff53aa1363f
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-04-25 13:23:15 +1200

    Convert App from functional component to class

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, Route } from "@react-navigation/native";
 import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
@@ -13,45 +13,84 @@ import { LatLng } from "react-native-maps";
 
 const Tab = createMaterialBottomTabNavigator();
 
-export default function App() {
-    const [activeRoutes, setActiveRoutes] = useState<RouteData[]>([]);
-    const [inactiveRoutes, setInctiveRoutes] = useState<RouteData[]>([]);
-    return (
-        <NavigationContainer>
-            <Tab.Navigator
-                initialRouteName="Map"
-                backBehavior="none" >
-                <Tab.Screen
-                    name="Map"
-                    options={{
-                        tabBarLabel: "Map",
-                        tabBarIcon: ({ color }) => <MaterialCommunityIcons name="map" color={color} size={24} />,
-                    }}>
-                    {() => <MapScreen activeRoutes={activeRoutes} />}
-                </Tab.Screen>
-                <Tab.Screen
-                    name="Routes"
-                    options={{
-                        tabBarLabel: "Routes",
-                        tabBarIcon: ({ color }) => <MaterialIcons name="search" color={color} size={24} />,
-                    }} >
-                    {() => <RoutesScreen
-                        activeRoutes={activeRoutes}
-                        addRoute={async newSR => { const newR = await loadRouteData(newSR); newR && setActiveRoutes([...activeRoutes, newR]) }}
-                        removeRoute={oldR => setActiveRoutes(activeRoutes.filter(r => r !== oldR))}
-                        updateRoute={(oldR, newR) => setActiveRoutes([...activeRoutes.filter(x => x !== oldR), newR])} />}
-                </Tab.Screen>
-                <Tab.Screen
-                    name="Settings"
-                    component={SettingsScreen}
-                    options={{
-                        tabBarLabel: "Settings",
-                        tabBarIcon: ({ color }) => <MaterialIcons name="settings" color={color} size={24} />,
-                    }}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+interface AppProps { }
+
+interface AppState {
+    activeRoutes: RouteData[];
+    inactiveRoutes: RouteData[];
+}
+
+export default class App extends React.Component<AppProps, AppState> {
+    state: AppState = {
+        activeRoutes: [],
+        inactiveRoutes: [],
+    }
+
+    render = () => {
+        const { activeRoutes, inactiveRoutes } = this.state;
+
+        return (
+            <NavigationContainer>
+                <Tab.Navigator
+                    initialRouteName="Map"
+                    backBehavior="none" >
+                    <Tab.Screen
+                        name="Map"
+                        options={{
+                            tabBarLabel: "Map",
+                            tabBarIcon: ({ color }) => <MaterialCommunityIcons name="map" color={color} size={24} />,
+                        }}>
+                        {() => <MapScreen activeRoutes={activeRoutes} />}
+                    </Tab.Screen>
+                    <Tab.Screen
+                        name="Routes"
+                        options={{
+                            tabBarLabel: "Routes",
+                            tabBarIcon: ({ color }) => <MaterialIcons name="search" color={color} size={24} />,
+                        }} >
+                        {() => <RoutesScreen
+                            activeRoutes={activeRoutes}
+                            addRoute={this.addRoute}
+                            removeRoute={this.removeRoute}
+                            updateRoute={this.updateRoute} />}
+                    </Tab.Screen>
+                    <Tab.Screen
+                        name="Settings"
+                        component={SettingsScreen}
+                        options={{
+                            tabBarLabel: "Settings",
+                            tabBarIcon: ({ color }) => <MaterialIcons name="settings" color={color} size={24} />,
+                        }}
+                    />
+                </Tab.Navigator>
+            </NavigationContainer>
+        );
+    }
+
+    setActiveRoutes = (activeRoutes: RouteData[]) => {
+        this.setState({ activeRoutes });
+    }
+
+    setInactiveRoutes = (inactiveRoutes: RouteData[]) => {
+        this.setState({ inactiveRoutes });
+    }
+
+    addRoute = async (newSR: SearchRouteData) => {
+        const newR = await loadRouteData(newSR);
+        if (newR === null) {
+            console.error("Failed loading data for searchRoute", newSR);
+            return;
+        } 
+        this.setActiveRoutes([...this.state.activeRoutes, newR]);
+    }
+
+    updateRoute = (oldR: RouteData, newR: RouteData) => {
+        this.setActiveRoutes(this.state.activeRoutes.map(r => r === oldR ? newR : r));
+    }
+
+    removeRoute = (oldR: RouteData) => {
+        this.setActiveRoutes(this.state.activeRoutes.filter(r => r !== oldR));
+    }
 }
 
 const styles = StyleSheet.create({
