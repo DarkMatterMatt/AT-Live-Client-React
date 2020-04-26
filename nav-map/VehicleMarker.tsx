8c3d5c30657b2f23118739fe0d6ed8c4356c0cbe
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-04-26 20:26:09 +1200

    Animate markers moving

--- nav-map/VehicleMarker.tsx
+++ nav-map/VehicleMarker.tsx
@@ -1,11 +1,12 @@
-import React, { useState, useRef } from "react";
-import { Marker, LatLng } from "react-native-maps";
-import { StyleSheet, View, Dimensions, Platform, ImageURISource } from "react-native";
-import { encode } from "base-64";
-import VehicleData from "../types/VehicleData";
-import TransitType from "../types/TransitType";
-import MarkerIcon from "../common/MarkerIcon";
+import React, { useRef, useState } from "react";
+import { Animated, Easing } from "react-native";
+import { AnimatedRegion, Marker } from "react-native-maps";
 import { bezierBlend, clamp } from "../common/helpers";
+import MarkerIcon from "../common/MarkerIcon";
+import TransitType from "../types/TransitType";
+import VehicleData from "../types/VehicleData";
+
+const AnimatedMarker = Animated.createAnimatedComponent(Marker);
 
 interface VehicleMarkerProps {
     vehicle: VehicleData;
@@ -15,13 +16,35 @@ interface VehicleMarkerProps {
 }
 
 export default function VehicleMarker({ vehicle, zIndex, type, color }: VehicleMarkerProps) {
+    const { id, direction, timestamp, position } = vehicle;
+
     const lastIconProps = useRef("");
+    const [lastPosition, setLastPosition] = useState(position);
+    const coordinate = useRef(new AnimatedRegion({
+        ...position,
+        // never used, fix tsc warning
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+    }));
+
+    // animate position changes
+    if (lastPosition.latitude !== position.latitude || lastPosition.longitude !== position.longitude) {
+        setLastPosition(position);
+
+        // TODO: make duration a setting
+        // Android should support non-JS animations using animateMarkerToCoordinate but it doesn't :(
+        coordinate.current.timing({
+            ...position,
+            duration: 1000,
+            easing: Easing.inOut(Easing.quad),
+        }).start();
+    }
 
     // TODO: change constants to setting variables
-    let opacity = 1 - 0.7 * bezierBlend((Date.now() - vehicle.timestamp - 20) / (90 - 20));
+    let opacity = 1 - 0.7 * bezierBlend((Date.now() - timestamp - 20) / (90 - 20));
     opacity = clamp(0, 1, opacity);
 
-    const dotFill = vehicle.direction ? "#FFF" : "#000";
+    const dotFill = direction ? "#FFF" : "#000";
 
     // rerender marker if any props change
     let renderMarker = false;
@@ -32,12 +55,12 @@ export default function VehicleMarker({ vehicle, zIndex, type, color }: VehicleM
 
     // TODO: allow changing between marker icons and bus/rail/ferry icons
     return (
-        <Marker
+        <AnimatedMarker
             tracksViewChanges={renderMarker}
-            zIndex={zIndex}
-            identifier={vehicle.id}
-            coordinate={vehicle.position} >
+            style={{ zIndex }}
+            identifier={id}
+            coordinate={coordinate.current} >
             <MarkerIcon height={32} fill={color} dotFill={dotFill} opacity={opacity} />
-        </Marker>
+        </AnimatedMarker>
     );
 }
